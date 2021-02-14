be6185576d155421a1e2be3dae504709bd625ab0
vipin cheriyanveetil <vipinc.007@gmail.com>
2021-02-14 20:31:44 +0530

    initial commit

--- src/service/open_weather.js
+++ src/service/open_weather.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 ("use strict");
 export default class DataAPI {
-  API_URL = null;
+  API_URL_ONECALL = null;
   API_KEY = "c1b05123eea170e5538e5c0219680474";
   constructor() {
-    this.API_URL = "https://api.openweathermap.org/data/2.5/onecall";
+    this.API_URL_ONECALL = "https://api.openweathermap.org/data/2.5/onecall";
   }
   doGet(apiname) {
     return axios
-      .get(this.API_URL + apiname + "&appid=" + this.API_KEY)
+      .get(this.API_URL_ONECALL + apiname + "&appid=" + this.API_KEY)
       .then((response) => {
         this.results = response.data;
         this.results.RequestSuccess = true;
--- src/service/utils.js
+++ src/service/utils.js
@@ -7,7 +7,6 @@ export default class Utils {
     localStorage.setItem(this.USER_LOCATION_TOKEN, JSON.stringify(userobject));
   }
   static get_saved_location() {
-    console.log("key name", this.USER_LOCATION_TOKEN);
     var returnObjName = JSON.parse(
       localStorage.getItem(this.USER_LOCATION_TOKEN)
     );
