34e2bd9cc0160525c1e57e5896e1c98d5d51e3ad
vipin cheriyanveetil <vipinc.007@gmail.com>
2021-02-14 20:04:25 +0530

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,18 +2,19 @@
   <div class="hello">
     <h1>Weather information</h1>
     <places
-      v-model="form.country.label"
-      placeholder="Where are we going ?"
+      v-model="currentSearchedPlace.country.label"
+      placeholder="search any location ?"
       @change="onLocationChange"
-      :options="options"
+      :options="placesSearchOptions"
     >
     </places>
 
-    <div v-if="errorStr">
-      Sorry, but the following error occurred: {{ errorStr }}
+    <div v-if="errorWhileFindingYourLocation">
+      Sorry, but the following error occurred:
+      {{ errorWhileFindingYourLocation }}
     </div>
 
-    <div v-if="gettingLocation">
+    <div v-if="isFindingYourLocation">
       <i>Getting your location...</i>
     </div>
 
@@ -28,7 +29,7 @@
     </div>
     <br />
     <div>
-      <select class="form-control" v-model="drpView" required>
+      <select class="form-control" v-model="currentWeatherView" required>
         <option value="current">Current</option>
         <option value="hourly">Hourly</option>
         <option value="daily">Daily</option>
@@ -39,22 +40,15 @@
       <table style="width:100%; text-align: left;">
         <tr>
           <th width="30%">dt</th>
-          <th width="30%">
-            high / Low
-          </th>
+          <th width="30%">high / Low</th>
           <th width="30%">condition</th>
         </tr>
-        <!-- {{
-        getSeletcedWeatherView()
-      }} -->
         <tr
-          v-for="item in getSeletcedWeatherView()"
-          :key="JSON.stringify(item) + drpView"
+          v-for="item in getWeatherInfoByCurrentWeatherView()"
+          :key="JSON.stringify(item) + currentWeatherView"
         >
-          <td>
-            {{ getDateDetails(item) }}
-          </td>
-          <td>{{ getTempDetails(item.temp) }}</td>
+          <td>{{ getFormatedDateToDisplay(item) }}</td>
+          <td>{{ getTemperatureToDisplay(item.temp) }}</td>
           <td>{{ item.weather[0].main }}</td>
         </tr>
       </table>
@@ -63,8 +57,6 @@
 </template>
 
 <script>
-// eslint-disable-next-line no-unused-vars
-
 import Utils from "@/service/utils.js";
 import Places from "vue-places";
 export default {
@@ -75,20 +67,19 @@ export default {
   },
   data: function() {
     return {
-      firstName: "test",
-      lastName: "wow",
-      address: "",
       location: null,
-      gettingLocation: false,
-      errorStr: null,
-      drpView: "current",
+      isFindingYourLocation: false,
+      errorWhileFindingYourLocation: null,
+      currentWeatherView: "current",
       weatherdata: null,
-      options: {
+      placesSearchOptions: {
         appId: "pl76CO106DSQ",
         apiKey: "18183ac7abce5ce0e583f6674330e092",
+        aroundLatLngViaIP: true,
+        useDeviceLocation: true,
         // countries: ["US"],
       },
-      form: {
+      currentSearchedPlace: {
         country: {
           label: null,
           data: {},
@@ -99,18 +90,18 @@ export default {
   created() {
     //do we support geolocation
     if (!("geolocation" in navigator)) {
-      this.errorStr = "Geolocation is not available.";
+      this.errorWhileFindingYourLocation = "Geolocation is not available.";
       return;
     }
 
-    this.gettingLocation = true;
+    this.isFindingYourLocation = true;
     // get position
     let saved_location = Utils.get_saved_location();
 
     if (saved_location === null) {
       navigator.geolocation.getCurrentPosition(
         (pos) => {
-          this.gettingLocation = false;
+          this.isFindingYourLocation = false;
           this.location = pos;
 
           Utils.save_location({
@@ -123,96 +114,51 @@ export default {
           );
         },
         (err) => {
-          this.gettingLocation = false;
-          this.errorStr = err.message;
+          this.isFindingYourLocation = false;
+          this.errorWhileFindingYourLocation = err.message;
         }
       );
     } else {
-      this.gettingLocation = false;
+      this.isFindingYourLocation = false;
       this.load_weather(saved_location.latitude, saved_location.longitude);
     }
-    //Utils.clear_storage();
-  },
-  mounted: function() {
-    this.$nextTick(function() {
-      // this.load_weather();
-      // this.$refs.address.focus();
-      // for (let ref in this.$refs) {
-      //   eval("new google.maps.places.Autocomplete('" + this.$refs[ref] + "')");
-      // }
-    });
   },
   methods: {
     onLocationChange: function(newval) {
       console.log(newval);
+      if (Object.keys(newval).length == 0) return;
       this.load_weather(newval.latlng.lat, newval.latlng.lng, true);
     },
-    getDateDetails: function(item) {
+    getFormatedDateToDisplay: function(item) {
       return new Date(
         (item.dt + this.weatherdata.timezone_offset) * 1000
       ).toLocaleString("en-US");
     },
-    getTempDetails: function(temp) {
+    getTemperatureToDisplay: function(temp) {
       if (temp instanceof Object) {
         return `${temp.min} / ${temp.max}`;
       } else return temp;
     },
-    getAddressData: function(addressData) {
-      this.address = addressData;
-      // let k = placeResultData;
-      // let idd = id;
-    },
-    getSeletcedWeatherView: function() {
-      if (this.weatherdata === null) return [];
 
-      if (this.drpView == "current") {
+    getWeatherInfoByCurrentWeatherView: function() {
+      if (this.weatherdata === null) return [];
+      if (this.currentWeatherView == "current") {
         let arr = [];
-        arr.push(this.weatherdata.current);
+        arr.push({ ...this.weatherdata.current });
         return arr;
       } else {
-        return this.weatherdata[this.drpView];
+        return this.weatherdata[this.currentWeatherView];
       }
     },
-    timeConverter: function(UNIX_timestamp) {
-      var a = new Date(UNIX_timestamp * 1000);
-      var months = [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec",
-      ];
-      var year = a.getFullYear();
-      var month = months[a.getMonth()];
-      var date = a.getDate();
-      var hour = a.getHours();
-      var min = a.getMinutes();
-      var sec = a.getSeconds();
-      var time =
-        date + " " + month + " " + year + " " + hour + ":" + min + ":" + sec;
-      return time;
-    },
-    getFullName: function() {
-      const t = "ttt";
-      let k = "sdfsdf";
-      return this.firstName + " " + this.lastName + t + k;
-    },
 
-    load_weather: function(lat, long, save_preference = false) {
+    load_weather: function(latitude, longitude, save_location = false) {
       this.$dataAPI
-        .doGet("?lat=" + lat + "&lon=" + long + "&exclude=minutely,alerts")
+        .doGet(`?lat=${latitude}&lon=${longitude}&exclude=minutely,alerts`)
         .then((results) => {
           if (results.RequestSuccess) {
             console.log(results);
             this.weatherdata = results;
-            if (save_preference)
+            if (save_location)
               Utils.save_location({
                 latitude: results.lat,
                 longitude: results.lon,
